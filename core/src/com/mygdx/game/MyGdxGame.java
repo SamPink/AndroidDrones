11636783595dcae087ed30a25be21c60df5b419c
SamPink <42603236+SamPink@users.noreply.github.com>
2020-01-30 18:19:12 +0000

    added comments
    comments explaining the code
    
    isVisable now only returns true of is in view

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -3,6 +3,7 @@ package com.mygdx.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.PerspectiveCamera;
@@ -27,7 +28,7 @@ public class MyGdxGame extends ApplicationAdapter {
 	public AssetManager assets;
 	public Array<ModelInstance> instances = new Array<ModelInstance>();
 	public Environment environment;
-	public boolean loading;
+	public boolean loading; // loading flag used to determine if asset is in memory
 
 	public Array<ModelInstance> blocks = new Array<ModelInstance>();
 	public Array<ModelInstance> invaders = new Array<ModelInstance>();
@@ -43,21 +44,26 @@ public class MyGdxGame extends ApplicationAdapter {
 
 	@Override
 	public void create () {
-		modelBatch = new ModelBatch();
-		environment = new Environment();
-		environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1f));
+
+		modelBatch = new ModelBatch(); //used to store game models
+		environment = new Environment(); //stores the game world
+		environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1f)); //sets lighting conditions
 		environment.add(new DirectionalLight().set(0.8f, 0.8f, 0.8f, -1f, -0.8f, -0.2f));
 
-		cam = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		//creates game camera and view position
+		cam = new PerspectiveCamera(50, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 		cam.position.set(0f, 7f, 10f);
 		cam.lookAt(0,0,0);
 		cam.near = 1f;
 		cam.far = 300f;
 		cam.update();
 
+		//implements default controller input
+		//TODO needs updated
 		camController = new CameraInputController(cam);
 		Gdx.input.setInputProcessor(camController);
 
+		//class all assets are stored within
 		assets = new AssetManager();
 		assets.load("ship.g3db", Model.class);
 		assets.load("block.obj", Model.class);
@@ -65,13 +71,16 @@ public class MyGdxGame extends ApplicationAdapter {
 		assets.load("spacesphere.obj", Model.class);
 		loading = true;
 
-		stage = new Stage();
-		font = new BitmapFont();
-		label = new Label(" ", new Label.LabelStyle(font, Color.WHITE));
-		stage.addActor(label);
+		stage = new Stage(); //HUD element
+		label = new Label(" ", new Label.LabelStyle(new BitmapFont(), Color.WHITE));//create empty label
+		stage.addActor(label); //add label to screen
 		stringBuilder = new StringBuilder();
 	}
 
+	/**
+	 * loads assets into memory
+	 * spawning objects
+	 */
 	private void doneLoading() {
 		ship = new ModelInstance(assets.get("ship.g3db", Model.class));
 		ship.transform.setToRotation(Vector3.Y, 180).trn(0, 0, 6f);
@@ -97,23 +106,24 @@ public class MyGdxGame extends ApplicationAdapter {
 
 		space = new ModelInstance(assets.get("spacesphere.obj", Model.class));
 
-		loading = false;
+		loading = false; //loading is false when objects are in memory
 	}
 
 	@Override
 	public void render () {
-		if (loading && assets.update())
-			doneLoading();
-		camController.update();
+		if (loading && assets.update()) //true if objects are not in memory or is objects have changed
+			doneLoading();//load into memory
+		camController.update();//update the view
 
+		//clear canvas
 		Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
 
 		modelBatch.begin(cam);
 		visibleCount = 0;
-		for (final ModelInstance instance : instances) {
+		for (final ModelInstance instance : instances) {//all objects in game
 			if (isVisible(cam, instance)) {
-				modelBatch.render(instance, environment);
+				modelBatch.render(instance, environment);//object is viable in current view
 				visibleCount++;
 			}
 		}
@@ -121,6 +131,7 @@ public class MyGdxGame extends ApplicationAdapter {
 			modelBatch.render(space);
 		modelBatch.end();
 
+		//write components to HUD
 		stringBuilder.setLength(0);
 		stringBuilder.append(" FPS: ").append(Gdx.graphics.getFramesPerSecond());
 		stringBuilder.append(" Visible: ").append(visibleCount);
@@ -128,8 +139,10 @@ public class MyGdxGame extends ApplicationAdapter {
 		stage.draw();
 	}
 
-	private boolean isVisible(PerspectiveCamera cam, ModelInstance instance) {
-		return true; // FIXME: Implement frustum culling
+	protected boolean isVisible(final Camera cam, final ModelInstance instance) {
+		Vector3 position = new Vector3();
+		instance.transform.getTranslation(position);
+		return cam.frustum.pointInFrustum(position);
 	}
 
 	@Override
